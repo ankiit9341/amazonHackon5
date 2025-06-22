75c2c22da7582678a8870217dc498391fcd566d7
Ankit Kumar Yadav <itsankit9341@gmail.com>
2025-06-23 03:58:13 +0530

    Normalize all line endings to LF

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/amazonHackon5/',           // ← add this line
   plugins: [react()],
   server: {
     hmr: {
